a3df926acd618c5b85ae79464b172ff24a31cd6d
Daypops <71375552+Daypops@users.noreply.github.com>
2026-02-11 19:39:33 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,8 +1,15 @@
-const config = JSON.parse(localStorage.getItem("shopConfig")) || {};
+let config = {};
 
-document.getElementById("title").value = config.title || "";
-document.getElementById("bgColor").value = config.bgColor || "#f5f5f5";
-document.getElementById("buttonColor").value = config.buttonColor || "#0078ff";
+// Charger config existante
+fetch("https://3634-2a01-cb0d-294-e200-9eb-d022-9b6d-e6aa.ngrok-free.app/get-config")
+  .then(res => res.json())
+  .then(data => {
+    config = data;
+
+    document.getElementById("title").value = config.title || "";
+    document.getElementById("bgColor").value = config.bgColor || "#f5f5f5";
+    document.getElementById("buttonColor").value = config.buttonColor || "#0078ff";
+  });
 
 document.getElementById("logo").addEventListener("change", e => {
   const reader = new FileReader();
@@ -17,6 +24,16 @@ function save() {
   config.bgColor = document.getElementById("bgColor").value;
   config.buttonColor = document.getElementById("buttonColor").value;
 
-  localStorage.setItem("shopConfig", JSON.stringify(config));
-  alert("Paramètres enregistrés ✅");
+  fetch("https://3634-2a01-cb0d-294-e200-9eb-d022-9b6d-e6aa.ngrok-free.app/save-config", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(config)
+  })
+  .then(res => res.json())
+  .then(() => {
+    alert("Paramètres sauvegardés globalement ✅");
+  })
+  .catch(err => console.error(err));
 }
